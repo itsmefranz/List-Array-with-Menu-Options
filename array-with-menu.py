c213b3d3d76df1a2bf600ebdf103feada29bd291
itsmefranz <daniellafrancine.velasquez@gmail.com>
2022-11-05 10:17:50 +0800

    started with printing the list of values in my program

---
+++ array-with-menu.py
@@ -0,0 +1,11 @@
+#Write a python program that do the following:
+#   - display the initial content of the array
+#   - display a menu of options
+#   - allow user to select item in the menu (check if valid)
+#   - perform the selected option (you may prompt additional info to user when need)
+#   - display the resulting values of the array
+
+print(".・。.・゜✭・.・✫・゜・。.")
+list_values=[5, 6, 7, 8, 9, 12, 14, 19, 20, 21]
+#displays the content of the array
+print(list_values)
