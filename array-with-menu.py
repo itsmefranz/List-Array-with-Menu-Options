3c3f0fbceab9ebecc4bb32f37b2da37f41419b5c
itsmefranz <daniellafrancine.velasquez@gmail.com>
2022-11-05 10:25:07 +0800

    finished the menu display will now move onto infusing conditions with them

--- array-with-menu.py
+++ array-with-menu.py
@@ -8,4 +8,15 @@
 print(".・。.・゜✭・.・✫・゜・。.")
 list_values=[5, 6, 7, 8, 9, 12, 14, 19, 20, 21]
 #displays the content of the array
-print(list_values)
+menu=True
+while menu:
+    print ("""
+    Menu
+    [1] Add an element
+    [2] Insert an element
+    [3] Modify an element
+    [4] Delete an element
+    [5] Arrange in ascending order
+    [6] Arrange in descending order
+    """)
+    menu=input("What would you like to do? ")
