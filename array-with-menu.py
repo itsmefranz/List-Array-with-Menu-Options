2037f37ebcdc47408726ba8c1c1dd9e0659fae74
itsmefranz <daniellafrancine.velasquez@gmail.com>
2022-11-05 10:56:09 +0800

    settled conditions for menu 1 and 2 but yet to make menu 2 function

--- array-with-menu.py
+++ array-with-menu.py
@@ -8,15 +8,27 @@
 print(".・。.・゜✭・.・✫・゜・。.")
 list_values=[5, 6, 7, 8, 9, 12, 14, 19, 20, 21]
 #displays the content of the array
+print(list_values)
 menu=True
 while menu:
     print ("""
     Menu
-    [1] Add an element
-    [2] Insert an element
-    [3] Modify an element
-    [4] Delete an element
-    [5] Arrange in ascending order
-    [6] Arrange in descending order
+    [1] ❀ Add an element
+    [2] ✎ Insert an element
+    [3] ✭ Modify an element
+    [4] ✂ Delete an element
+    [5] ▲ Arrange in ascending order
+    [6] ▼ Arrange in descending order
     """)
     menu=input("What would you like to do? ")
+    if menu=="1":
+        add_value=input("What number would you like to add to the existing list? ")
+        list_values.append(add_value)
+
+        print(f'Updated List {list_values}')
+    elif menu=="2":
+        insert_value=input("Please input a number and an index you would like to put your number at: ")
+        list_values.insert(insert_value)
+
+        print(f'Updated List {list_values}')
+        
